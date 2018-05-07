1ce287fe29cb08b14946e188fca5553654ab99d9
Ekapop <eploentham@gmail.com>
2018-05-07 11:53:00 +0700

    1
    1

--- BackupDatabase/InitConfig.cs
+++ BackupDatabase/InitConfig.cs
@@ -11,5 +11,6 @@ namespace BackupDatabase
         public String hostDB1 = "", userDB1 = "", passDB1 = "", nameDB1 = "", portDB1 = "", pathBackupDatabase="";
         public String hostDB2 = "", userDB2 = "", passDB2 = "", nameDB2 = "", portDB2 = "";
         public String hostDB3 = "", userDB3 = "", passDB3 = "", nameDB3 = "", portDB3 = "";
+        public String startBackupTime = "";
     }
 }
